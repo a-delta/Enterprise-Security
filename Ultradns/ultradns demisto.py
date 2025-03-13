e6a81a6226661b44773ba852c4cd89f5ff2e4723
Ademola <131720330+a-delta@users.noreply.github.com>
2025-03-13 11:18:38 +0000

    Update ultradns demisto.py

--- Ultradns/ultradns demisto.py
+++ Ultradns/ultradns demisto.py
@@ -1,4 +1,4 @@
-register_module_line('Ultradns:Zone Checker - Tesco -v1', 'start', __line__())
+register_module_line('Ultradns:Zone Checker -v1', 'start', __line__())
 ### pack version = 3.0.15'
 '''IMPORTS'''
 import requests
@@ -24,16 +24,32 @@ def get_auth_token(login_url, username, password):
         print(f"Error getting auth token: {e}")
         return None
 
-# Function to make authenticated API call and collect response
-def fetch_data(url, token):
+# Function to check zone records within zone and return response
+def get_zone_props(api_url, token):
     headers = {"Authorization": f"Bearer {token}"}
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(api_url, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return None
+    
+# Function to check domain records within zone and return response
+def get_domain_props():
+    headers = {"Authorization": f"Bearer {token}"}
+    api_url = api_url + domain
+        #Define args
+    args = set_args()
+    domain = args[0]
+
+# Function to make authenticated API call and collect zone response
+def get_ip_props():
+    headers = {"Authorization": f"Bearer {token}"}
+    api_url = api_url + ip
+    #Define args
+    args = set_args()
+    ip = args[0]
 
 # Function to store data in a JSON file
 def store_data(data, filename="api_response.json"):
@@ -50,3 +66,36 @@ if __name__ == "__main__":
         data = fetch_data(API_URL, token)
         if data:
             store_data(data)
+
+def test_module():
+    api_token = generate_auth_token()
+
+    params = {'clusterId': clusterId}
+    headers = {'authorization': f"Bearer {api_token}"}
+    endpointURL = api_url + "orgSafeList?"
+
+    response = requests.get(endpointURL, params=params, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        return_results('ok')
+    else:
+        error_msg = f'Error in API call [{response.status_code}] - {response.reason}'
+        return_error(error_msg)
+
+''' COMMANDS MANAGER / SWITCH PANEL '''
+
+try:
+    if demisto.command() == 'get_domain_props':
+        get_domain_props()
+    elif demisto.command() == 'get_zone_props':
+        get_zone_props()
+    elif demisto.command() == 'get_ip_props':
+        get_ip_props()
+    elif demisto.command() == 'test-module':
+        # This is the call made when pressing the integration test button.
+        test_module()
+
+except Exception as err:
+    return_error(str(err))
+
+register_module_line('Ultradns:Zone Checker -v1', 'end', __line__())
